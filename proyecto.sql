d09c8638b8da1af95f60ccef501a9bce33b18120
ThickEmperor <61020543+ThickEmperor@users.noreply.github.com>
2021-11-25 21:41:11 -0600

    Update proyecto.sql

--- proyecto.sql
+++ proyecto.sql
@@ -1,5 +1,5 @@
 Create Table Usuarios(
-Idd Int Not Null,
+IdU Varchar2(6) Not Null,--@@@###
 Nombres Varchar2 (40) Not Null,
 ApePat Varchar2(40) Not Null,
 ApeMat Varchar2(40)Not Null,
@@ -7,21 +7,25 @@ Matricula Varchar2(12) Not Null,
 Tipo Varchar2(10) Not Null,
 Fecha Date Not Null,
 NumQuizzes Int,
-Constraint PK$IDD$USUARIOS Primary Key(Idd),
+Email Varchar2(80),
+Contrasenia Varchar2(30)
+Constraint PK$IDU$USUARIOS Primary Key(IdU),
 Constraint UK$Matricula$Usuarios Unique(Matricula)
 );
+--------------------------------------------------
 Create Table Quiz(
-Idd Int Not Null,
+IdQ Varchar2(6) Not Null,--QZ####
 Nombre Varchar2 (40) Not Null,
 NumPreguntas Int Not Null,
 Fecha Date Not Null,
-Creador Int Not Null,
-Constraint PK$IDD$QUIZ Primary Key (Idd),
-Constraint FK$CREADOR$USUARIOS Foreign Key (Creador) References Usuarios(Idd)
+Creador Varchar2(6) Not Null,--@@@###
+Constraint PK$IDQ$QUIZ Primary Key (IdQ),
+Constraint FK$CREADOR$USUARIOS Foreign Key (Creador) References Usuarios(IdU)
 );
+-----------------------------------------------------
 Create Table Preguntas(
-Idd Int Not Null,
-IdQ Int Not Null,
+IdP Varchar2(6) Not Null,--PR####
+IdQ Varchar2(6) Not Null,--@@@###
 NumPregunta Int Not Null,
 DescPregunta Varchar2(80) Not Null,
 TipoPregunta Varchar2(15) Not Null,
@@ -30,19 +34,40 @@ Opc2 Varchar2(80),
 Opc3 Varchar2(80),
 Opc4 Varchar2(80),
 Respuesta Varchar2(80),
-Constraint PK$IDD$PREGUNTAS Primary Key (Idd),
-Constraint FK$IDQ$QUIZ Foreign Key (IdQ) References Quiz(Idd)
+Constraint PK$IDP$PREGUNTAS Primary Key (IdP),
+Constraint FK$IDQ$QUIZ Foreign Key (IdQ) References Quiz(IdQ)
 );
+----------------------------------------------------------
 Create Table Resultados(
-Idd Int Not Null,
+IdR Varchar2(6) Not Null,--QR####
 Usuario Int Not Null,
 Quizz Int Not Null,
 Pregunta Int Not Null,
 Fecha Date Not Null,
 Respuesta Varchar2(80),
 Resultado Varchar2(10),
-Constraint PK$IDD$RESULTADOS Primary Key(Idd),
-Constraint FK$USUARIO$USUARIOS Foreign Key(Usuario) References Usuarios(Idd),
-Constraint FK$QUIZZ$QUIZ Foreign Key(Quizz) References Quiz(Idd),
-Constraint FK$PREGUNTA$PREGUNTAS Foreign Key(Pregunta) References Preguntas(Idd)
+Constraint PK$IDR$RESULTADOS Primary Key(IdR),
+Constraint FK$USUARIO$USUARIOS Foreign Key(Usuario) References Usuarios(IdU),
+Constraint FK$QUIZZ$QUIZ Foreign Key(Quizz) References Quiz(IdQ),
+Constraint FK$PREGUNTA$PREGUNTAS Foreign Key(Pregunta) References Preguntas(IdP)
 );
+-------------------------------------------------------
+Create Table ResultadosQuizz(
+IdRQ Varchar2(6),--RQ####
+IdQ Varchar2(6),--QZ####
+Calificaion Number,--En el juego es precisión
+Correcto Int,
+Incorrecto Int,
+Puntuacion Int,
+TiempoRlz Time,
+Correctas Int,
+Incorrectas Int,
+Racha Int,
+Constraint PK$IDQR$RESULTADOSQUIZZ Primary Key(IdRQ),
+Constraint FK$RESULTADOSQUIZZ$QUIZZ Foreign Key (IdQ) References Quiz(IdQ)
+);
+---------------------------------------------------------
+
+
+Select Substr(Nombre, 1, 1)|| Substr(ApePat, 1, 1) ||Substr(ApeMat, 1, 1)
+from tabla where idd = 1--Función generadora de ID
