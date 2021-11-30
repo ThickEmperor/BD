2affb3775d2b1537c65e3a4cd358bd838475f220
ThickEmperor <61020543+ThickEmperor@users.noreply.github.com>
2021-11-30 05:13:51 -0600

    Add files via upload

---
+++ QUIZZ PREGUNTA.sql
@@ -0,0 +1,21 @@
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta1','Abierta');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta2','Multiple');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta3','V/F');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta4','Check');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta5','Abierta');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta6','Multiple');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta7','V/F');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta8','Check');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta9','Abierta');
+Insert Into P_QUIZZ_PREGUNTA (ID_QUIZZ,PREGUNTA,TIPO)Values
+('QZZ2','Pregunta10','Multiple');
+Select * From P_Quizz_pregunta
---
+++ USUARIOS.sql
@@ -0,0 +1,21 @@
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Victoria','Hernandez','Flores','Victoria@default.com','Victoria123','Alumno');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Renata','Garcia','Cruz','Renata@default.com','Renata123','Alumno');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Sofia','Lopez','Ramirez','Sofia@default.com','Sofia123','Profesor');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Valeria','Gonzales','Sanchez','Valeria@default.com','Valeria123','Alumno');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Ximena','Perez','Rodriguez','Ximena@default.com','Ximena123','Alumno');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Alexander','Rodriguez','Perez','Alexander@default.com','Alexander123','Alumno');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Mateo','Sanchez','Gonzales','Mateo@default.com','Mateo123','Alumno');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Santiago','Ramirez','Lopez','Santiago@default.com','Santiago123','Alumno');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Daniel','Cruz','Garcia','Daniel@default.com','Daniel123','Profesor');
+Insert Into P_USUARIO (NOMBRE,APEPAT,APEMAT,EMAIL,CONTRASENIA,PERFIL)Values 
+('Sebastian','Flores','Hernandez','Sebastina@default.com','Sebastian123','Alumno');
+Select * From P_Usuario 
